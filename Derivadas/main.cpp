18156deabf0e793f7632ddd1298350d1a053d466
Eduardo Aguilera <eduardoaguilera@Eduardos-MacBook-Air.local>
2013-03-18 18:43:02 -0600

    Initial Commit

---
+++ Derivadas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Derivadas
+//
+//  Created by Eduardo Aguilera on 3/18/13.
+//  Copyright (c) 2013 Eduardo Aguilera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
